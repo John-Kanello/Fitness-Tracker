00aa2e2baea87f4e261c0734bb452293b20dcb62
John-Kanello <ioanniskanello@gmail.com>
2025-03-25 18:14:51 +0200

    Added FitnessAppUserMapper to convert users from dto to entity and vice versa

---
+++ src/fitnesstracker/service/FitnessAppUserMapper.java
@@ -0,0 +1,33 @@
+package fitnesstracker.service;
+
+import fitnesstracker.model.dto.request.FitnessAppUserRequestDto;
+import fitnesstracker.model.dto.response.FitnessAppUserResponseDto;
+import fitnesstracker.model.entity.FitnessAppUser;
+import fitnesstracker.util.mapper.Mapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FitnessAppUserMapper implements Mapper<FitnessAppUser, FitnessAppUserRequestDto, FitnessAppUserResponseDto> {
+    private final PasswordEncoder passwordEncoder;
+
+    public FitnessAppUserMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public FitnessAppUser toEntity(FitnessAppUserRequestDto dtoRequest) {
+        return new FitnessAppUser(
+                dtoRequest.email(),
+                passwordEncoder.encode(dtoRequest.password())
+        );
+    }
+
+    @Override
+    public FitnessAppUserResponseDto toDtoResponse(FitnessAppUser entity) {
+        return new FitnessAppUserResponseDto(
+                entity.getId(),
+                entity.getEmail()
+        );
+    }
+}

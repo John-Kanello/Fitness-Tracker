976c6a633e0410f3df768513f5c7d9ab2efdef55
John-Kanello <ioanniskanello@gmail.com>
2025-03-25 16:24:02 +0200

    Added jakarta validation annotations for FitnessTrackerRequestDto

--- src/fitnesstracker/model/dto/request/FitnessTrackerRequestDto.java
+++ src/fitnesstracker/model/dto/request/FitnessTrackerRequestDto.java
@@ -1,8 +1,14 @@
 package fitnesstracker.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record FitnessTrackerRequestDto(
+        @NotBlank
         String username,
+        @NotBlank
         String activity,
+        @NotBlank
         int duration,
+        @NotBlank
         int calories
 ) {}

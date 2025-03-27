740faf7976b026d75bc1878f73bc0e4a4482d80b
John-Kanello <ioanniskanello@gmail.com>
2025-03-27 17:57:52 +0200

    Added ApiKeyAuthentication to ensure ApiKeyAuthenticationProvider only authenticates instances of this class

---
+++ src/fitnesstracker/security/ApiKeyAuthentication.java
@@ -0,0 +1,51 @@
+package fitnesstracker.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class ApiKeyAuthentication implements Authentication {
+    private final String apiKey;
+    private boolean isAuthenticated = false;
+
+    public ApiKeyAuthentication(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return apiKey;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return "Api key token";
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        this.isAuthenticated = isAuthenticated;
+    }
+
+    @Override
+    public String getName() {
+        return "Api key token";
+    }
+}

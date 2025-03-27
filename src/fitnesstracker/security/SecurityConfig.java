d593d552a65445e94f32fcb50c72d01a9a35defd
John-Kanello <ioanniskanello@gmail.com>
2025-03-27 17:59:56 +0200

    Registered new ApiKeyAuthenticationProvider, ApiKeyConfigurer and explicitly added FitnessAppUserDetailsService as the UserDetailsService implementation

--- src/fitnesstracker/security/SecurityConfig.java
+++ src/fitnesstracker/security/SecurityConfig.java
@@ -12,6 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final ApiKeyAuthenticationProvider apiKeyAuthenticationProvider;
+    private final FitnessAppUserDetailsService fitnessAppUserDetailsService;
+
+    public SecurityConfig(ApiKeyAuthenticationProvider apiKeyAuthenticationProvider, FitnessAppUserDetailsService fitnessAppUserDetailsService) {
+        this.apiKeyAuthenticationProvider = apiKeyAuthenticationProvider;
+        this.fitnessAppUserDetailsService = fitnessAppUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,9 +29,11 @@ public class SecurityConfig {
                         .requestMatchers("/actuator/shutdown").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/api/developers/signup").permitAll()
-                        .requestMatchers("/api/tracker").permitAll()
                         .anyRequest().authenticated()
                 )
+                .with(new ApiKeyConfigurer(), Customizer.withDefaults())
+                .authenticationProvider(apiKeyAuthenticationProvider)
+                .userDetailsService(fitnessAppUserDetailsService)
                 .sessionManagement(sessions -> sessions
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
                 )

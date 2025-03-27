2cdaf9a5d096c9c2db56403b17c1eda39af4ebf9
John-Kanello <ioanniskanello@gmail.com>
2025-03-27 18:01:14 +0200

    FitnessTrackerController postTracker() now stores the application for the corresponding FitnessTracker

--- src/fitnesstracker/controller/FitnessTrackerController.java
+++ src/fitnesstracker/controller/FitnessTrackerController.java
@@ -3,11 +3,14 @@ package fitnesstracker.controller;
 import fitnesstracker.model.dto.request.FitnessTrackerRequestDto;
 import fitnesstracker.model.dto.response.FitnessTrackerResponseDto;
 import fitnesstracker.model.entity.FitnessTracker;
+import fitnesstracker.model.entity.UserApplication;
 import fitnesstracker.service.FitnessTrackerService;
+import fitnesstracker.service.UserApplicationService;
 import fitnesstracker.util.mapper.FitnessTrackerMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,15 +19,24 @@ import java.util.List;
 @RequestMapping("/api/tracker")
 public class FitnessTrackerController {
     private final FitnessTrackerService fitnessTrackerService;
+    private final UserApplicationService userApplicationService;
     private final FitnessTrackerMapper fitnessTrackerMapper;
 
-    public FitnessTrackerController(FitnessTrackerService fitnessTrackerService, FitnessTrackerMapper fitnessTrackerMapper) {
+    public FitnessTrackerController(FitnessTrackerService fitnessTrackerService,
+                                    UserApplicationService userApplicationService,
+                                    FitnessTrackerMapper fitnessTrackerMapper) {
         this.fitnessTrackerService = fitnessTrackerService;
+        this.userApplicationService = userApplicationService;
         this.fitnessTrackerMapper = fitnessTrackerMapper;
     }
 
     @PostMapping
-    public ResponseEntity<?> postTracker(@Valid @RequestBody FitnessTrackerRequestDto fitnessTrackerRequestDto) {
+    public ResponseEntity<?> postTracker(@Valid @RequestBody FitnessTrackerRequestDto fitnessTrackerRequestDto,
+                                         Authentication authentication) {
+        String apiKey = authentication.getCredentials().toString();
+        UserApplication userApplication = userApplicationService.findByApiKey(apiKey)
+                .orElseThrow();
+        fitnessTrackerRequestDto.setApplication(userApplication.getName());
         FitnessTracker fitnessTracker = fitnessTrackerMapper.toEntity(fitnessTrackerRequestDto);
         fitnessTrackerService.save(fitnessTracker);
         return new ResponseEntity<>(HttpStatus.CREATED);

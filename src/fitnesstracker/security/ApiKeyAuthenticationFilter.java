1e1d7ff8825a3110c2464806aadb7c10bef079a8
John-Kanello <ioanniskanello@gmail.com>
2025-03-28 16:12:12 +0200

    Removed RequestMatcher matcher from ApiKeyAuthenticationFilter as the matching is handled at SecurityFilterChain level

--- src/fitnesstracker/security/ApiKeyAuthenticationFilter.java
+++ src/fitnesstracker/security/ApiKeyAuthenticationFilter.java
@@ -11,16 +11,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
     private final AuthenticationManager authenticationManager;
-    private final RequestMatcher requestMatcher =
-            new AntPathRequestMatcher("/api/tracker/**");
     private final AuthenticationEntryPoint authenticationEntryPoint = ((request,
                                                                         response,
                                                                         authException) -> {
@@ -36,22 +32,18 @@ public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        if(requestMatcher.matches(request)) {
-            try {
-                var apiKey = request.getHeader("X-API-Key");
-                if(apiKey != null) {
-                    Authentication authentication = new ApiKeyAuthentication(apiKey);
-                    authentication = authenticationManager.authenticate(authentication);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-                throw new BadCredentialsException("No api key was provided...");
-            } catch (AuthenticationException e) {
-                authenticationEntryPoint.commence(request, response, e);
+        try {
+            var apiKey = request.getHeader("X-API-Key");
+            if(apiKey != null) {
+                Authentication authentication = new ApiKeyAuthentication(apiKey);
+                authentication = authenticationManager.authenticate(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                filterChain.doFilter(request, response);
                 return;
             }
+            throw new BadCredentialsException("No api key was provided...");
+        } catch (AuthenticationException e) {
+            authenticationEntryPoint.commence(request, response, e);
         }
-        filterChain.doFilter(request, response);
     }
 }

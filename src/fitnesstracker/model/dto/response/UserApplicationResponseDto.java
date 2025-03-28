6bc28e0e2319b890ea490b05c41e23b2d3b41ed9
John-Kanello <ioanniskanello@gmail.com>
2025-03-28 16:51:24 +0200

    Created UserApplicationMapper for mapping UserApplication entity to dto response

--- src/fitnesstracker/model/dto/response/UserApplicationResponseDto.java
+++ src/fitnesstracker/model/dto/response/UserApplicationResponseDto.java
@@ -3,7 +3,9 @@ package fitnesstracker.model.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record UserApplicationResponseDto(
+        long id,
         String name,
+        String description,
         @JsonProperty(value = "apikey")
         String apiKey
 ) {}
--- src/fitnesstracker/util/mapper/UserApplicationMapper.java
+++ src/fitnesstracker/util/mapper/UserApplicationMapper.java
@@ -1,31 +1,17 @@
 package fitnesstracker.util.mapper;
 
-import fitnesstracker.model.dto.request.UserApplicationRequestDto;
 import fitnesstracker.model.dto.response.UserApplicationResponseDto;
 import fitnesstracker.model.entity.UserApplication;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Component
-public class UserApplicationMapper implements
-        RequestMapper<UserApplication, UserApplicationRequestDto>,
-        ResponseMapper<UserApplication, UserApplicationResponseDto> {
-    @Override
-    public UserApplication toEntity(UserApplicationRequestDto dtoRequest) {
-        return new UserApplication(
-                dtoRequest.name(),
-                dtoRequest.description(),
-                UUID.randomUUID().toString(),
-                LocalDateTime.now()
-        );
-    }
-
+public class UserApplicationMapper implements ResponseMapper<UserApplication, UserApplicationResponseDto> {
     @Override
     public UserApplicationResponseDto toResponseDto(UserApplication entity) {
         return new UserApplicationResponseDto(
+                entity.getId(),
                 entity.getName(),
+                entity.getDescription(),
                 entity.getApiKey()
         );
     }

a58e16ca79468a2c7e8f8658ca3de2b895954d60
John-Kanello <ioanniskanello@gmail.com>
2025-03-28 16:52:55 +0200

    Modified UserApplicationController to use new request and response dtos for better clarity

--- src/fitnesstracker/controller/UserApplicationController.java
+++ src/fitnesstracker/controller/UserApplicationController.java
@@ -1,12 +1,12 @@
 package fitnesstracker.controller;
 
-import fitnesstracker.model.dto.request.UserApplicationRequestDto;
-import fitnesstracker.model.dto.response.UserApplicationResponseDto;
+import fitnesstracker.model.dto.request.UserApplicationRegistrationRequestDto;
+import fitnesstracker.model.dto.response.UserApplicationRegistrationResponseDto;
 import fitnesstracker.model.entity.FitnessAppUser;
 import fitnesstracker.model.entity.UserApplication;
 import fitnesstracker.service.FitnessAppUserService;
 import fitnesstracker.service.UserApplicationService;
-import fitnesstracker.util.mapper.UserApplicationMapper;
+import fitnesstracker.util.mapper.UserApplicationRegistrationMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,31 +21,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApplicationController {
     private final UserApplicationService userApplicationService;
     private final FitnessAppUserService fitnessAppUserService;
-    private final UserApplicationMapper userApplicationMapper;
+    private final UserApplicationRegistrationMapper userApplicationRegistrationMapper;
 
     public UserApplicationController(UserApplicationService userApplicationService,
                                      FitnessAppUserService fitnessAppUserService,
-                                     UserApplicationMapper userApplicationMapper) {
+                                     UserApplicationRegistrationMapper userApplicationRegistrationMapper) {
         this.userApplicationService = userApplicationService;
         this.fitnessAppUserService = fitnessAppUserService;
-        this.userApplicationMapper = userApplicationMapper;
+        this.userApplicationRegistrationMapper = userApplicationRegistrationMapper;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserApplicationResponseDto> registerApplication(@Valid @RequestBody UserApplicationRequestDto userApplicationRequestDto,
-                                                                          Authentication authentication) {
+    public ResponseEntity<UserApplicationRegistrationResponseDto> registerApplication(@Valid @RequestBody UserApplicationRegistrationRequestDto userApplicationRequestDto,
+                                                                                      Authentication authentication) {
         String currentUserEmail = authentication.getName();
         if(!fitnessAppUserService.existsByEmail(currentUserEmail)) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         FitnessAppUser fitnessAppUser = fitnessAppUserService.findByEmail(currentUserEmail)
                 .orElseThrow();
-        UserApplication userApplication = userApplicationMapper.toEntity(userApplicationRequestDto);
+        UserApplication userApplication = userApplicationRegistrationMapper.toEntity(userApplicationRequestDto);
         userApplication.setFitnessAppUser(fitnessAppUser);
         fitnessAppUser.getApplications().add(userApplication);
         UserApplication savedUserApplication = userApplicationService.save(userApplication);
         return new ResponseEntity<>(
-                userApplicationMapper.toResponseDto(savedUserApplication),
+                userApplicationRegistrationMapper.toResponseDto(savedUserApplication),
                 HttpStatus.CREATED
         );
     }

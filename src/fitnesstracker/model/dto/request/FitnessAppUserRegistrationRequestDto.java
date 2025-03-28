f9f7208db0f7054e6e02b3d0d4691279d52b19dc
John-Kanello <ioanniskanello@gmail.com>
2025-03-28 16:47:59 +0200

    Renamed FitnessAppUserRequestDto to FitnessAppUserRegistrationRequestDto

--- src/fitnesstracker/model/dto/request/FitnessAppUserRegistrationRequestDto.java
+++ src/fitnesstracker/model/dto/request/FitnessAppUserRegistrationRequestDto.java
@@ -3,7 +3,7 @@ package fitnesstracker.model.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record FitnessAppUserRequestDto(
+public record FitnessAppUserRegistrationRequestDto(
         @NotBlank
         @Email(regexp = ".+[@].+[\\.].+")
         String email,

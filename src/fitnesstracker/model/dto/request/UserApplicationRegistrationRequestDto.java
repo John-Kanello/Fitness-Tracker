250b74cdd680cba4717bdce408baf0f7599fce3e
John-Kanello <ioanniskanello@gmail.com>
2025-03-28 16:49:33 +0200

    Renamed UserApplicationRequestDto to UserApplicationRegistrationRequestDto

--- src/fitnesstracker/model/dto/request/UserApplicationRegistrationRequestDto.java
+++ src/fitnesstracker/model/dto/request/UserApplicationRegistrationRequestDto.java
@@ -3,10 +3,10 @@ package fitnesstracker.model.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserApplicationRequestDto(
+public record UserApplicationRegistrationRequestDto(
         @NotBlank
         String name,
         @NotNull
         String description
-) { }
+) {}
 
